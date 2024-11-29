730afd7b378505e5b7c23469d0d6062d4bb76ba1
PetryakovPavel <petryakovpavelvl@gmail.com>
2024-11-29 18:33:03 +0300

    Add files via upload

---
+++ precache-manifest.e271d77946d5f051b0f4415a7aae834e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f1c658796a5bef08327055fcc52b2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d9ba41ebb50fd6aa870",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "0d9ba41ebb50fd6aa870",
+    "url": "/static/js/main.406b4c1e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
