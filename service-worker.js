1475629aa4866b8926f01591ae6ef102fef6aeaf
PetryakovPavel <petryakovpavelvl@gmail.com>
2024-11-30 16:26:18 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,16 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
@@ -12,44 +25,15 @@ self.addEventListener('message', (event) => {
 
 workbox.core.clientsClaim();
 
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-// Прокси для API Яндекс.Переводчика
-workbox.routing.registerRoute(
-  /https:\/\/translate\.api\.cloud\.yandex\.net\/translate\/v2\/translate/,
-  async ({ request }) => {
-    const apiKey = 'dict.1.1.20241129T153507Z.d8c1dcd9753a57dc.85a8eed0320168213a1874ab822582205226f41f'; // Замените на ваш API-ключ
-
-    // Получаем текст и язык из тела запроса
-    const { text, targetLanguageCode } = await request.json();
-
-    try {
-      const response = await fetch(`https://translate.api.cloud.yandex.net/translate/v2/translate`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Api-Key ${apiKey}`
-        },
-        body: JSON.stringify({
-          targetLanguageCode,
-          texts: [text],
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-      }
-
-      return response.json(); // Возвращаем JSON-ответ
-    } catch (error) {
-      console.error('Translation error:', error);
-      return new Response('Error translating text', { status: 500 });
-    }
-  }
-);
-
-// Регистрация маршрута навигации
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/practice_5/index.html"), {
-  blacklist: [/^\/_/, /\/[^/]+\.[^/]+$/],
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
